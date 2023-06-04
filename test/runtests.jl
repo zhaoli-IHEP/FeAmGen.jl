215205e3caad2601ab78676af3a10b24e1d5a8be
Quan-feng WU <32387646+Fenyutanchan@users.noreply.github.com>
2023-06-04 21:56:46 +0800

    add runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,18 @@
-run_all_tests.jl
+
+include("basicTest.jl")
+
+include("ggttbar_Test.jl")
+
+# include("DrellYan_Test.jl")
+
+# include("eeHZ_Test.jl")
+
+# include("gbtw_Test.jl")
+
+# include("tWb_Test.jl")
+
+include("tWtW_Test.jl")
+
+include("ggttbar_CT_Test.jl")
+
+include("test_construct_topology.jl")
